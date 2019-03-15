ae1d73ad0a2af093908a7c075b540091c306d28e
th3Ch4OT1C <benfire811@gmail.com>
2019-03-15 13:14:21 -0700

    + news feed (latest release)

--- javascript/news.js
+++ javascript/news.js
@@ -1,5 +1,28 @@
 $(window).ready(function() {
 	$.get('https://api.github.com/repos/endless-sky/endless-sky/releases/latest', function(data) {
-		console.log(data.body);
+		$("#release").text(data.name);
+		$("#latest").html(parseData(data.body));
+		console.log(data.name);
 	});
 });
+
+function parseData(data) {
+	let i = 0;
+	while (data.indexOf('![') > -1 && i < 10) {
+		let subdata = data.substring(data.indexOf('!['), data.length);
+		let link = subdata.substring(subdata.indexOf('!['), subdata.indexOf(')') + 1);
+		let name = link.substring(link.indexOf('![') + 1, link.indexOf(']'));
+		let href = link.substring(link.indexOf('(') + 1, link.indexOf(')'));
+		data = data.replace(link, '<img src="' + href + '" default="' + name + '">');
+		i++;
+	}
+	while (data.indexOf('[') > -1 && i < 10) {
+		let subdata = data.substring(data.indexOf('['), data.length);
+		let link = subdata.substring(subdata.indexOf('['), subdata.indexOf(')') + 1);
+		let name = link.substring(link.indexOf('[') + 1, link.indexOf(']'));
+		let href = link.substring(link.indexOf('(') + 1, link.indexOf(')'));
+		data = data.replace(link, '<a href="' + href + '">' + name + "</a>");
+		i++;
+	}
+	return data;
+}
